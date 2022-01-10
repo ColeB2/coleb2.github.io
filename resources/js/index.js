eee7fa88525fbc36849f4813b3f696041f24f366
Cole <cebCole@gmail.com>
2022-01-10 04:55:01 -0600

    commented out script, removed unused code

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,9 +1,1 @@
-window.onload = () => {
-                const birthYear = 1995;
-                const currentYear = new Date().getFullYear();
-                const age =
-                    new Date() > new Date(`01/04/${currentYear}`)
-                        ? currentYear - birthYear
-                        : currentYear - birthYear - 1;
-                document.getElementById("age").innerText = `${age}`;
-            };
+//window.onload = () => {};
